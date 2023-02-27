ae507777bdbf263d7553a4585cbab15c4b00d593
LuisAngel2k21 <luiscarranza198@yahoo.com>
2023-02-27 14:39:31 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1.4/Ejercicio1.4/Model/ListViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1_4.Model
+{
+    internal class ListViewModel
+    {
+
+        #region Attributes
+        public object listViewSource;
+        #endregion
+
+        #region Properties
+        public object ListViewSource { get { return listViewSource; } set { listViewSource = value; } }
+        //set { setValue(ref listViewSource, value);  } }
+        #endregion
+
+        #region Methods
+        public async Task LoadData()
+        {
+            ListViewSource = await App.BD.ListaImagen();
+        }
+        #endregion
+
+        #region Constructor
+        public ListViewModel()
+        {
+            LoadData();
+        }
+        #endregion
+    }
+}
